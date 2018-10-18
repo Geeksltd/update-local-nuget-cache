8933aa347886bb29179c8d22b8c42ed9729a0178
Ashkan Saeedi Mazdeh <ashkan_gc@yahoo.com>
2018-10-18 15:39:09 +0330

    Cleaned it up

--- update-local-nuget-cache/Program.cs
+++ update-local-nuget-cache/Program.cs
@@ -15,19 +15,18 @@ namespace update_local_nuget_cache
                 return 1;
             }
 
-            string projectPath = args[0];
-            string outputFile = args[1];
-            string projectName = args[2];
-
+            var projectPath = args[0];
+            var outputFile = args[1];
+            var projectName = args[2];
 
             var data = XElement.Load(projectPath);
             Console.WriteLine("---");
 
-            //we get it like this since other property groups might exist with similar elements like version
-            XElement propertyGroup = data.Descendants("OutputType").First().Parent;
-            string version = propertyGroup.Element("Version")?.Value;
-            string targetFramework = propertyGroup.Element("TargetFramework")?.Value;
-            string packageId = propertyGroup.Element("PackageId")?.Value;
+            // we get it like this since other property groups might exist with similar elements like version
+            var propertyGroup = data.Descendants("OutputType").First().Parent;
+            var version = propertyGroup.Element("Version")?.Value;
+            var targetFramework = propertyGroup.Element("TargetFramework")?.Value;
+            var packageId = propertyGroup.Element("PackageId")?.Value;
             if (string.IsNullOrEmpty(version))
                 version = "1.0.0";
             if (string.IsNullOrEmpty(packageId))
@@ -35,25 +34,27 @@ namespace update_local_nuget_cache
 
             var userProfile = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
             var nuGetCache = userProfile + "\\.nuget\\packages\\";
-            string packageFolder = nuGetCache + packageId + "\\" + version + "\\lib\\" + targetFramework;
+            var packageFolder = nuGetCache + packageId + "\\" + version + "\\lib\\" + targetFramework;
 
             if (!Directory.Exists(packageFolder))
             {
                 Console.WriteLine("did not exist : " + packageFolder);
                 return 0;
             }
+
             Console.WriteLine("***");
             Console.WriteLine(packageFolder);
             var index = outputFile.LastIndexOf("\\");
             Console.WriteLine(index);
-            string fileName = outputFile.Substring(index + 1, outputFile.Length - (index + 1));
-            string nugetFilePath = packageFolder + "\\" + fileName;
+            var fileName = outputFile.Substring(index + 1, outputFile.Length - (index + 1));
+            var nugetFilePath = packageFolder + "\\" + fileName;
             Console.WriteLine("**");
             Console.WriteLine(nugetFilePath);
             if (File.Exists(nugetFilePath))
             {
                 File.Delete(nugetFilePath);
             }
+
             File.Copy(outputFile, packageFolder + "\\" + fileName);
             return 0;
         }
