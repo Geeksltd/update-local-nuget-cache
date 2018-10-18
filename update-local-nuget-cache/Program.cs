ec4ed7e07d3329f47719d5c55340013401afdcc0
Ashkan Saeedi Mazdeh <ashkan_gc@yahoo.com>
2018-10-18 19:25:19 +0330

    added support for package.nuspec file

--- update-local-nuget-cache/Program.cs
+++ update-local-nuget-cache/Program.cs
@@ -11,26 +11,43 @@ namespace update_local_nuget_cache
         {
             if (args.Length < 3)
             {
-                Console.WriteLine("The project directory should be sent");
+                Console.WriteLine("Syntax error\nShould be called like this:\nupdate-local-nuget-cache $(ProjectPath) $(OutputPath) $(ProjectName)");
                 return 1;
             }
 
             var projectPath = args[0];
+            var projectDir = Path.GetDirectoryName(projectPath);
             var outputFile = args[1];
             var projectName = args[2];
+            var nuspecPath = Path.Combine(projectDir, "Package.nuspec");
+            var hasNuSpec = File.Exists(nuspecPath);
 
-            var data = XElement.Load(projectPath);
-            Console.WriteLine("---");
+            var csProjXml = XElement.Load(projectPath);
 
             // we get it like this since other property groups might exist with similar elements like version
-            var propertyGroup = data.Descendants("OutputType").First().Parent;
-            var version = propertyGroup.Element("Version")?.Value;
+            var propertyGroup = csProjXml.Descendants("OutputType").First().Parent;
             var targetFramework = propertyGroup.Element("TargetFramework")?.Value;
-            var packageId = propertyGroup.Element("PackageId")?.Value;
-            if (string.IsNullOrEmpty(version))
-                version = "1.0.0";
-            if (string.IsNullOrEmpty(packageId))
-                packageId = projectName;
+            var version = "";
+            var packageId = "";
+
+            if (!hasNuSpec)
+            {
+                version = propertyGroup.Element("Version")?.Value;
+                packageId = propertyGroup.Element("PackageId")?.Value;
+                if (string.IsNullOrEmpty(version))
+                    version = "1.0.0";
+                if (string.IsNullOrEmpty(packageId))
+                    packageId = projectName;
+            }
+            else
+            {
+                var nuSpecXml = XElement.Load(nuspecPath);
+                // The nuspec file has a namespace so we need to use it in our operations
+                var ns = nuSpecXml.GetDefaultNamespace();
+                var metadata = nuSpecXml.Descendants(ns + "metadata").First();
+                version = metadata.Element(ns + "version").Value;
+                packageId = metadata.Element(ns + "id").Value;
+            }
 
             var userProfile = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
             var nuGetCache = userProfile + "\\.nuget\\packages\\";
@@ -42,20 +59,15 @@ namespace update_local_nuget_cache
                 return 0;
             }
 
-            Console.WriteLine("***");
-            Console.WriteLine(packageFolder);
-            var index = outputFile.LastIndexOf("\\");
-            Console.WriteLine(index);
-            var fileName = outputFile.Substring(index + 1, outputFile.Length - (index + 1));
-            var nugetFilePath = packageFolder + "\\" + fileName;
-            Console.WriteLine("**");
-            Console.WriteLine(nugetFilePath);
+            var fileName = Path.GetFileName(outputFile);
+            var nugetFilePath = Path.Combine(packageFolder, fileName);
+
             if (File.Exists(nugetFilePath))
             {
                 File.Delete(nugetFilePath);
             }
 
-            File.Copy(outputFile, packageFolder + "\\" + fileName);
+            File.Copy(outputFile, Path.Combine(packageFolder, fileName));
             return 0;
         }
     }
