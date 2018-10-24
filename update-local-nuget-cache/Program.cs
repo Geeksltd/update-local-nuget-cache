4d3a07f0e8949454b30211e908b3294bea34422d
Ashkan Saeedi Mazdeh <ashkan_gc@yahoo.com>
2018-10-24 19:42:15 +0330

    Fixed a bug where pathes were being calculated incorrectly

--- update-local-nuget-cache/Program.cs
+++ update-local-nuget-cache/Program.cs
@@ -53,10 +53,18 @@ namespace update_local_nuget_cache
             var userProfile = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
             var nuGetCache = userProfile + "\\.nuget\\packages\\";
             var packageTopFolder = nuGetCache + packageId;
-            var list = Directory.GetDirectories(packageTopFolder).Select(x => Path.GetDirectoryName(x)).ToList();
-            list.Sort();
-            if (list.Count > 0)
-                version = list[list.Count - 1];
+            //if we have different versions of the package
+            //let's find the latest and put the files in it
+            if (Directory.Exists(packageTopFolder))
+            {
+                //GetFileName gets the last part of the path which here will be directory names
+                var list = Directory.GetDirectories(packageTopFolder).Select(x => Path.GetFileName(x)).ToList();
+                //we sort the names so we get the latest one easily
+                list.Sort();
+                if (list.Count > 0)
+                    version = list[list.Count - 1];
+            }
+
             var packageFolder = packageTopFolder + "\\" + version + "\\lib\\" + targetFramework;
 
             if (!Directory.Exists(packageFolder))
