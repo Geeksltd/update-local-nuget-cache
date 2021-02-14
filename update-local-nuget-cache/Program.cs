9867208e8bad092531b16987b0296a1f638a7747
Geeksltd <master@geeks.ltd.uk>
2021-02-14 10:58:14 +0000

    general fixes

--- update-local-nuget-cache/Program.cs
+++ update-local-nuget-cache/Program.cs
@@ -8,97 +8,127 @@ namespace update_local_nuget_cache
 {
     class Program
     {
-        static FileInfo Dll, Pdb, CsProj;
-        static DirectoryInfo LocalNugetCacheFolder;
-        static string TargetFramework;
-
-        static FileInfo FindCsProj()
-        {
-            var parent = Dll.Directory;
-
-            while (parent.Root != parent)
-            {
-                CsProj = parent.GetFiles("*.csproj")
-                  .OrderByDescending(v => v.Name.ToLower() == Dll.Name.Substring(0, Dll.Name.Length - 3) + "csproj")
-                  .FirstOrDefault();
-
-                if (CsProj != null) return CsProj;
-                parent = parent.Parent;
-            }
-
-            return null;
-        }
-
-        static void FindFiles(string arg)
-        {
-            Dll = arg.AsFile();
-            if (!Dll.Exists()) throw new Exception("File not found: " + Dll.FullName);
-            TargetFramework = Dll.Directory.Name;
-
-            Pdb = Dll.Directory.GetFile(Dll.NameWithoutExtension() + ".pdb");
-
-            if (FindCsProj() == null)
-                throw new Exception("CsProj file not found in the parents of " + Dll.FullName);
-        }
-
-        static void FindLocalNugetCache()
-        {
-            var dotnet = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles)
-                .AsDirectory().GetSubDirectory("dotnet").GetFile("dotnet.exe");
-
-            LocalNugetCacheFolder = dotnet.Execute("nuget locals global-packages -l").ToLines().Trim()
-                 .Select(v => v.TrimStart("global-packages: "))
-                 .Select(v => v.AsDirectory())
-                 .FirstOrDefault();
-
-            if (LocalNugetCacheFolder is null) throw new Exception("Nuget cache not found!");
-        }
+        static DirectoryInfo Root;
 
         static void Main(string[] args)
         {
-            if (args.Length != 1)
-                throw new Exception("Syntax: update-local-nuget-cache $(DllPath)");
+            Root = FindRoot(args.FirstOrDefault());
 
-            FindFiles(args.First());
+            foreach (var version in GetLocalCacheVersions(FindPackageId()))
+                Deploy(version);
+        }
 
+        static DirectoryInfo FindRoot(string firstArg)
+        {
+            if (firstArg.IsEmpty())
+                firstArg = Environment.CurrentDirectory;
 
+            DirectoryInfo result;
 
-            FindLocalNugetCache();
+            if (firstArg.AsFile().Exists())
+            {
+                result = FindParentDebugRelease(firstArg.AsFile().Directory);
+                return result ?? throw new Exception("No debug or release folder found for: " + firstArg);
+            }
 
-            var csProjXml = XElement.Load(CsProj.FullName);
-            var propertyGroup = csProjXml.Descendants("TargetFramework").First().Parent;
-            var packageId = propertyGroup.Element("PackageId").Value;
+            result = firstArg.AsDirectory();
+            if (!result.Exists()) throw new Exception("Directory not found: " + firstArg);
 
-            var folder = LocalNugetCacheFolder.GetSubDirectory(packageId);
+            var parent = FindParentDebugRelease(result);
+            if (parent != null) return parent;
+
+            result = result.GetSubDirectory("bin");
+            if (!result.Exists()) throw new Exception("Not found: " + result.FullName);
+
+            return new[] { result.GetSubDirectory("debug"), result.GetSubDirectory("release") }
+            .FirstOrDefault(x => x.Exists())
+            ?? throw new Exception("Didn't find debug or release in " + result.FullName);
+        }
+
+        static DirectoryInfo FindParentDebugRelease(DirectoryInfo folder)
+        {
+            while (true)
+            {
+                if (folder.Name.ToLower().IsAnyOf("debug", "release")) return folder;
+                folder = folder.Parent;
+                if (folder.FullName == folder.Root.FullName) return null;
+            }
+        }
+
+        static void Deploy(DirectoryInfo destination)
+        {
+            foreach (var target in destination.GetDirectories())
+            {
+                var newTarget = Root.GetDirectories()
+                    .FirstOrDefault(x => x.Name.ToLower().Remove(".") == target.Name.ToLower().Remove("."));
+
+                if (!newTarget.Exists())
+                {
+                    Console.WriteLine("Target " + target.Name + " not found in " + Root.FullName);
+                    continue;
+                }
+
+                foreach (var file in target.GetFiles())
+                {
+                    var here = newTarget.GetFile(file.Name);
+                    if (!here.Exists())
+                    {
+                        Console.WriteLine("Not found: " + here.FullName);
+                    }
+                    else
+                    {
+                        here.CopyTo(file, overwrite: true);
+                        Console.WriteLine("Updated " + file.FullName);
+                    }
+                }
+            }
+        }
+
+        static string FindPackageId()
+        {
+            var csProjXml = XElement.Load(FindCsProj().FullName);
+            return csProjXml.Descendants("PackageId").FirstOrDefault()?.Value
+                ?? throw new Exception("PackageId node not found in " + FindCsProj().FullName);
+        }
+
+        static FileInfo FindCsProj()
+        {
+            var parent = Root;
+
+            while (parent.Root.FullName != parent.FullName)
+            {
+                var result = parent.GetFiles("*.csproj").WithMax(v => v.LastWriteTimeUtc);
+
+                if (result != null) return result;
+                parent = parent.Parent;
+            }
+
+            throw new Exception("Csproj file not found for: " + Root.FullName);
+        }
+
+        static DirectoryInfo[] GetLocalCacheVersions(string packageId)
+        {
+            var folder = FindLocalNugetCache().GetSubDirectory(packageId);
 
             if (!folder.Exists())
             {
                 Console.WriteLine("Local installation not found: " + folder.FullName);
-                return;
+                return new DirectoryInfo[0];
             }
 
-            foreach (var version in folder.GetDirectories())
-            {
-                var target = version.GetSubDirectory("lib\\" + TargetFramework);
+            return folder.GetDirectories().Select(v => v.GetSubDirectory("lib")).Where(v => v.Exists()).ToArray();
+        }
 
-                if (!target.Exists())
-                {
-                    Console.WriteLine("Expected folder does not exists: " + target.FullName);
-                    continue;
-                }
+        static DirectoryInfo FindLocalNugetCache()
+        {
+            var dotnet = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles)
+                .AsDirectory().GetSubDirectory("dotnet").GetFile("dotnet.exe");
 
-                var targetFile = target.GetFile(Dll.Name);
-                if (!targetFile.Exists())
-                {
-                    Console.WriteLine("File folder does not exists: " + targetFile.FullName);
-                    continue;
-                }
-
-                Console.WriteLine("Copying to: " + targetFile.FullName);
-
-                Dll.CopyTo(targetFile, overwrite: true);
-                if (Pdb.Exists()) Pdb.CopyTo(targetFile.Directory.GetFile(Pdb.Name), overwrite: true);
-            }
+            return dotnet.Execute("nuget locals global-packages -l").ToLines().Trim()
+                 .Select(v => v.TrimStart("global-packages: "))
+                 .Select(v => v.AsDirectory())
+                 .FirstOrDefault()
+                 ?? throw new Exception("Nuget cache not found!");
         }
     }
 }
